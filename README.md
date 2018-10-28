f7063a3dc884db67bd41dc0e60de0a9eab3e546b
qiujiahong <qiujh@szfangle.com>
2018-10-28 10:11:07 +0800

    添加版本

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 该项目是一个基础镜像。包含了jdk1.8、redis 1哨兵，1主，2从，和一个tomcat 9 。 
 
 
+## 文件路径
+
+* /usr/local/redis
+* /usr/local/tomcat/
+
+
 ## docker hub地址
 
 https://hub.docker.com/r/fangle/java-tomcat-redis/
--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 
 echo "start build file"
 
-export name=java-tomcat-redis:v1.0
+export name=java-tomcat-redis
+export version=v1.0
 
-docker build --no-cache -t fangle/$name   .
+docker build --no-cache -t fangle/$name:$version   .
 
-# docker push fangle/$name
+docker tag fangle/$name:$version $name:$version:latest
+
+
+docker push fangle/$name:$version
+docker push fangle/$name:latest
 
 # docker run -d fangle/java-tomcat-redis:v1.0 tail -f /dev/null
